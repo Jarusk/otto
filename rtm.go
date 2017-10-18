61c8e846c7a9dfea9f9bedf6147c3697817274a5
Matt Yaraskavitch <yaraskavitch.matt@gmail.com>
2017-10-17 21:40:14 -0400

    Modified sample from slack golang library

--- rtm.go
+++ rtm.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	api := slack.New("API_KEY_OAUTH")
+	api := slack.New(os.Getenv("OTTO_SLACK_TOKEN"))
 	logger := log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)
 	slack.SetLogger(logger)
 	api.SetDebug(true)
@@ -18,28 +18,11 @@ func main() {
 	go rtm.ManageConnection()
 
 	for msg := range rtm.IncomingEvents {
-		fmt.Print("Event Received: ")
+		//fmt.Print("Event Received: ")
 		switch ev := msg.Data.(type) {
-		case *slack.HelloEvent:
-			// Ignore hello
-
-		case *slack.ConnectedEvent:
-			fmt.Println("Infos:", ev.Info)
-			fmt.Println("Connection counter:", ev.ConnectionCount)
-			// Replace #general with your Channel ID
-			rtm.SendMessage(rtm.NewOutgoingMessage("Hello world", "#general"))
 
 		case *slack.MessageEvent:
 			fmt.Printf("Message: %v\n", ev)
-
-		case *slack.PresenceChangeEvent:
-			fmt.Printf("Presence Change: %v\n", ev)
-
-		case *slack.LatencyReport:
-			fmt.Printf("Current latency: %v\n", ev.Value)
-
-		case *slack.RTMError:
-			fmt.Printf("Error: %s\n", ev.Error())
 
 		case *slack.InvalidAuthEvent:
 			fmt.Printf("Invalid credentials\n")
@@ -48,8 +31,6 @@ func main() {
 		default:
 
 			// Ignore other events..
-			// fmt.Printf("Unexpected: %v\n", msg.Data)
 		}
 	}
 }
-
